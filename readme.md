2bff4cf51ca4b1ef0d80d18b96d7735e9a966635
fafi09 <fafi09@126.com>
2020-01-08 15:10:58 +0800

    1.5 redis单击可以访问了

--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
 SpringCompensable.xml 为aspectj
 SpringConfig.xml 为spring-aop
+
+
+redis
+ps -ef | grep redis
+./redis-cli -h 127.0.0.1 -p 6379 shutdown
+./redis-server ../redis.conf
--- src/main/java/fafi/App.java
+++ src/main/java/fafi/App.java
@@ -3,6 +3,7 @@ import fafi.Impl.*;
 import fafi.pojo.User;
 import fafi.processor.MyBeanPostProcessor;
 import fafi.processor.MyBeanPostProcessor2;
+import fafi.redis.service.RedisService;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.cglib.beans.BeanGenerator;
 import org.springframework.context.ApplicationContext;
@@ -31,25 +32,25 @@ public class App
      * https://github.com/fafi09/SpringTest.git
      * @param args
      */
-    public static void main( String[] args ) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("SpringCompensable.xml");
-        CaptialService service = (CaptialService) context.getBean(CaptialService.class);
-        service.doRecord();
-
-        SubstractService substractService = (SubstractService) context.getBean("substractService");
-        int result = substractService.substract(5,2);
-        System.out.println(result);
-        CheckArithtectArgService checkService = (CheckArithtectArgService) substractService;
-        boolean ret = checkService.checkArithArg(5);
-        if(ret) {
-            System.out.println("check true-------------");
-        } else {
-            System.out.println("check false-------------");
-        }
-
-        User user = context.getBean(User.class);
-        System.out.println(user);
-    }
+//    public static void main( String[] args ) {
+//        ApplicationContext context = new ClassPathXmlApplicationContext("SpringCompensable.xml");
+//        CaptialService service = (CaptialService) context.getBean(CaptialService.class);
+//        service.doRecord();
+//
+//        SubstractService substractService = (SubstractService) context.getBean("substractService");
+//        int result = substractService.substract(5,2);
+//        System.out.println(result);
+//        CheckArithtectArgService checkService = (CheckArithtectArgService) substractService;
+//        boolean ret = checkService.checkArithArg(5);
+//        if(ret) {
+//            System.out.println("check true-------------");
+//        } else {
+//            System.out.println("check false-------------");
+//        }
+//
+//        User user = context.getBean(User.class);
+//        System.out.println(user);
+//    }
 
     /**
      * BeanFactory和ApplicationContext两个容器对待bean的后置处理器稍微有些不同。ApplicationContext容器会自动检测Spring配置文件中那些bean所对应的Java类实现了BeanPostProcessor接口，并自动把它们注册为后置处理器。在创建bean过程中调用它们，所以部署一个后置处理器跟普通的bean没有什么太大区别。
@@ -64,4 +65,9 @@ public class App
 //        User user = bf.getBean(User.class);
 //        System.out.println(user);
 //    }
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Spring-redis.xml");
+        RedisService service = (RedisService) context.getBean(RedisService.class);
+        service.set("pass","fsd");
+    }
 }
